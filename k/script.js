88b58951a5176ececdeb6112b5954cdbf6d14926
Gabovski21 <156270948+Gabovski21@users.noreply.github.com>
2024-02-26 18:47:24 -0600

    Update script.js

--- k/script.js
+++ k/script.js
@@ -1,73 +1,59 @@
-//Mostrar los personajes sin filtrar 
-//tenemos que obtener todos los personajes llamando a la api 
-//renderizarlos en nuestro dom 
-//cuando cambien los filtros se tiene que hacer un llamado nuevo a la api 
-//se debe renderizar nuevamente
+//Tenemos que obbtener todos los personajes llamado a la api
+//Renderizarlos en nuestro dom
+//Cuando cambien los filtros se tiene que hacer un llamado nuevo a la api
+//Se debe renderizar nuevamente
 
-const charactersEl = document.getElementById('characters');
+const characterEl = document.getElementById('characters');
 const nameFilterEl = document.getElementById('name-filter');
 const statusFilterEl = document.getElementById('status-filter');
 
-//tenemos que crear la funcion que haga el llamado a la api
+async function getCharacters(name, status) {
+    let url = 'https://rickandmortyapi.com/api/character';
 
-async function getCharacters (name, status){
+	if (name || status  ){
+		url += '?';
+		if(name){
+			url += `name=${name}&`;
+		}
 
-    let url = 'https://rickandmortyapi.com/api/character/';
-
-    if (name || status){
-        url += '?';
-        if(name){
-            url += `name=${name}&`;
-        }
-
-        if(status){
-            url += `status=${status}`;
-        }
-    }
-
-
+		if(status){
+			url += `status=${status}`
+		}
+	}
     const response = await fetch(url);
     const data = await response.json();
-
-    console.log(data.results);
-
+    
     return data.results;
-
 }
 
-// la funcion que a renderizar los elementos dentro del don
+async function displaycharacters(name, status) {
+    const characters = await getCharacters(name, status);
 
-async function displayCharacters(name, status){
+	characterEl.innerHTML='';
 
-// Obtener los personajes filtrados
-const characters = await getCharacters(name, status);
 
-charactersEl.innerHTML= '';
+    for (let character of characters) {
+        const card = document.createElement('div');
+        card.classList.add('character-card');
 
-// renderizar los personajes
-for( let character of characters ){
-    const card = document.createElement('div');
-card.classList.add('character-card');
+        card.innerHTML = `
+            <img src="${character.image}" alt="${character.name}"/>
+            <h2>${character.name}</h2>
+            <p>Status: ${character.status}</p>
+            <p>Species: ${character.species}</p>
+        `;
 
-card.innerHTML = `
-    <img src="${character.image}" />
-    <h2> ${character.name} </h2>
-    <p> Status: ${character.status} </p>
-    <p> Especie: ${character.species} </p>
-`;
-
-charactersEl.appendChild(card);
-
+        characterEl.appendChild(card);
+    }
 }
 
-}
+displaycharacters();
 
-displayCharacters();
 
-nameFilterEl.addEventListener('input', () =>  {
-    displayCharacters(nameFilterEl.value, statusFilterE.value );
+nameFilterEl.addEventListener('input',() =>{
+	displaycharacters(nameFilterEl.value, statusFilterEl.value );
 });
 
-statusFilterEl.addEventListener('change', ()=> {
-    displayCharacters(nameFilterEl.value, statusFilterEl.value );
+statusFilterEl.addEventListener('change', () => {
+    displaycharacters(nameFilterEl.value, statusFilterEl.value);
 });
